0120633f16d407d830fe7c28670069ad98ff626c
Muhammad Rizky Eka Arlin <rizkhy45@gmail.com>
2020-03-25 19:00:08 +0800

    added jumlah pasien, registrasi, dan sampel to dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Models\Patient;
+use App\Models\Registration;
+use App\Models\SampleTypeReceiveTakingPivot;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
@@ -23,6 +28,10 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('pages.dashboard.index');
+        return view('pages.dashboard.index', [
+            'patient_count' => Patient::count(),
+            'registration_count' => Registration::count(),
+            'sample_taken_count' => DB::table('sample_type_receive_taking_pivots')->count()
+        ]);
     }
 }
--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 
+@section('extend-css')
+    <style>
+        .border-statistic-width{
+            border-left-width: 0.5rem !important;
+        }
+    </style>
+@endsection
+
 @section('content')
 <!-- Content Header (Page header) -->
 <div class="content-header">
@@ -28,8 +36,22 @@
                             {{ session('status') }}
                         </div>
                     @endif
-            
+
                     Selamat datang {{ Auth::user()->name }}!
+                </div>
+                <div class="mt-4 d-flex flex-row align-content-stretch">
+                    <div class="media-body m-2 card p-3 border-left border-danger border-statistic-width">
+                        <h3 class="text-bold">Jumlah Pasien</h3>
+                        <p class="text-xl">{{ $patient_count }}</p>
+                    </div>
+                    <div class="media-body m-2 card p-3 border-left border-primary border-statistic-width">
+                        <h3 class="text-bold">Jumlah Registrasi</h3>
+                        <p class="text-xl">{{ $registration_count }}</p>
+                    </div>
+                    <div class="media-body m-2 card p-3 border-left border-info border-statistic-width">
+                        <h3 class="text-bold">Jumlah Sampel</h3>
+                        <p class="text-xl">{{ $sample_taken_count }}</p>
+                    </div>
                 </div>
             </div>
         </div>
